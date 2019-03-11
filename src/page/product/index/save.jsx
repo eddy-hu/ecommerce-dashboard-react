02c38715cde0599548ffa543e701b7691b236066
eddy-hu <eddy.hu@nokia.com>
2019-03-11 16:59:28 -0400

    add editor

--- src/page/product/index/save.jsx
+++ src/page/product/index/save.jsx
@@ -2,157 +2,205 @@ import React, { Component } from "react";
 import Header from "../../../component/header";
 import Util from "../../../util/index";
 import ProductService from "../../../service/product-service";
-import CategorySelector from './category-selector';
-import FileUploader from '../../../util/file-uploader/index';
-import './save.scss';
-
+import CategorySelector from "./category-selector";
+import FileUploader from "../../../util/file-uploader/index";
+import MyEditor from "../../../util/rich-editor/index";
+import "./save.scss";
 
 const productService = new ProductService();
 const util = new Util();
 
 class ProductSave extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            categoryId : 0,
-            parentCategoryId : 0,
-            subImages: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      title:'',
+      description:'',
+      price:'',
+      quantity:'',
+      detail:'',
+      status: 1, //1 means available
+      categoryId: 0,
+      parentCategoryId: 0,
+      subImages: [],
+    };
+  }
+  onCategoryChange(categoryId, parentCategoryId) {
+    //Get category ids;
+  }
+  onValueChange(e){
+    let name=e.target.name,
+        value=e.target.value.trim();
+    this.setState({
+      [name]: value
+    });
+  }
 
-        }
-    }
-    onCategoryChange(categoryId, parentCategoryId){
-        //Get category ids;
-    }
+  onDetailValueChange(value){
+    console.log(value);
+    this.setState({
+      detail:value
+    });
 
-    onUploadSuccess(res){
-      let subImages =  this.state.subImages;
-      subImages.push(res)
-      this.setState({
-        subImages: subImages,
-      })
+  }
 
-    }
-    onError(errMsg){
-      util.errorTips(errMsg);
-    }
+  getSubImagesString(){
+    return this.state.subImages.map((image) => image.url).join(',');
+  }
 
-    onImageDelete(e){
-      let index = e.target.index;
-      let subImages = this.state.subImages;
-      subImages.splice(index,1);
-      this.setState({
-        subImages: subImages
-      })
+  onSubmit(){
+    let product ={
+      title: this.state.title,
+      description: this.state.description,
+      price: this.state.price,
+      quantity: this.state.quantity,
+      detail: this.state.detail,
+      status: this.state.status,
+      categoryId: this.state.categoryId,
+      parentCategoryId: this.state.parentCategoryId,
+      subImages: this.state.subImages,
     }
+  }
+
+  onUploadSuccess(res) {
+    let subImages = this.state.subImages;
+    subImages.push(res);
+    this.setState({
+      subImages: subImages
+    });
+  }
+  onError(errMsg) {
+    util.errorTips(errMsg);
+  }
+
+  onImageDelete(e) {
+    let index = e.target.index;
+    let subImages = this.state.subImages;
+    subImages.splice(index, 1);
+    this.setState({
+      subImages: subImages
+    });
+  }
+
+  onEditorStateChange (editorState) {
+    console.log(this);
+    this.setState({
+      editorState,
+    });
+  }
 
   render() {
     return (
       <div id="page-wrapper">
         <Header title="Add Product" />
         <div>
-
           <div className="form-group row">
-            <label  className="col-md-2 col-form-label">
-              Title
-            </label>
+            <label className="col-md-2 col-form-label">Title</label>
             <div className="col-md-5">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Enter the product title"
+                name="title"
+                onChange={(e) =>this.onValueChange(e)}
               />
             </div>
           </div>
 
           <div className="form-group row">
-            <label className="col-md-2 col-form-label">
-              Description
-            </label>
+            <label className="col-md-2 col-form-label">Description</label>
             <div className="col-md-8">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Enter the product description"
+                name="description"
+                onChange={(e) =>this.onValueChange(e)}
               />
             </div>
           </div>
 
           <div className="form-group row">
-            <label  className="col-md-2 col-form-label">
-              Category
-            </label>
-            <CategorySelector onCategoryChange={
-                (categoryId, parentCategoryId) => this.onCategoryChange(categoryId, parentCategoryId)}/>
+            <label className="col-md-2 col-form-label">Category</label>
+            <CategorySelector
+              onCategoryChange={(categoryId, parentCategoryId) =>
+                this.onCategoryChange(categoryId, parentCategoryId)
+              }
+            />
           </div>
 
           <div className="form-group row">
-            <label  className="col-md-2 col-form-label">
-              Price
-            </label>
+            <label className="col-md-2 col-form-label">Price</label>
             <div className="col-md-3">
-            <div className="input-group-prepend">
-            <span className="input-group-text">$</span>
-            <input
-                type="number"
-                className="form-control"
-                placeholder="Enter the product price"
-              />
-            </div>
-             
+              <div className="input-group-prepend">
+                <span className="input-group-text">$</span>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="0"
+                  name="price"
+                  onChange={(e) =>this.onValueChange(e)}
+                />
+              </div>
             </div>
           </div>
 
           <div className="form-group row">
-            <label  className="col-md-2 col-form-label">
-              Quantity 
-            </label>
+            <label className="col-md-2 col-form-label">Quantity</label>
             <div className="col-md-3">
               <input
                 type="number"
                 className="form-control"
-                placeholder="Enter the product price"
+                placeholder="0"
+                name="quantity"
+                onChange={(e) =>this.onValueChange(e)}
               />
             </div>
           </div>
 
           <div className="form-group row">
-            <label  className="col-md-2 col-form-label">
-              Images 
-            </label>
+            <label className="col-md-2 col-form-label">Images</label>
             <div className="col-md-10">
-              {
-                this.state.subImages.length ? this.state.subImages.map(
-                  (image, index) => (
+              {this.state.subImages.length ? (
+                this.state.subImages.map((image, index) => (
                   <div className="img-con" key={index}>
-                  <img className="img" src={image.url}/>
-                  <i className="fa fa-close" index={index} onClick={(e)=> this.onImageDelete(e)}></i>
-                   </div>
-                  )
-                ) : (<div>Please upload image</div>)
-              }
+                    <img className="img" src={image.url} />
+                    <i
+                      className="fa fa-close"
+                      index={index}
+                      onClick={e => this.onImageDelete(e)}
+                    />
+                  </div>
+                ))
+              ) : (
+                <div>Please upload image</div>
+              )}
             </div>
             <div className="col-md-offset-2 col-md-10">
-              <FileUploader 
-              onSuccess={(res)=>{this.onUploadSuccess(res)}}
-              onError={(errMsg)=>{this.onUploadError(errMsg)}}
+              <FileUploader
+                onSuccess={res => {
+                  this.onUploadSuccess(res);
+                }}
+                onError={errMsg => {
+                  this.onUploadError(errMsg);
+                }}
               />
             </div>
           </div>
 
-          
           <div className="form-group row">
-            <label  className="col-md-2 col-form-label">
-              Detail 
-            </label>
+            <label className="col-md-2 col-form-label">Detail</label>
             <div className="col-md-10">
-              xxxx
+              <MyEditor 
+               onChange={(value) =>this.onDetailValueChange(value)}
+              />
             </div>
           </div>
 
           <div className="form-group row">
             <div className="col-md-10">
               <button type="submit" className="btn btn-primary">
-               Save
+                Save
               </button>
             </div>
           </div>
